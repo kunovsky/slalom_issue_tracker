014e6aba06d216bd31a16ef06ecedb2c600f9eff
Ty Kunovsky <tyson.kunovsky@slalom.com>
2015-11-23 15:32:59 -0600

    pointing this at heroku

--- app/config/config.js
+++ app/config/config.js
@@ -1,5 +1,5 @@
 export function config() {
-  let baseUrl = 'http://localhost:3000/';
+  let baseUrl = 'https://murmuring-savannah-3366.herokuapp.com/';
   return {
     issues: baseUrl + 'api/issues',
     resources: baseUrl + 'api/slalom_resources'
